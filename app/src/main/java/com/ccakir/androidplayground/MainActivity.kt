3f999e2c9a4115f4de3554cc55fc3081549d489f
Cemil Çakır <ccakir3719@gmail.com>
2021-05-24 20:49:32 +0300

    During startup, the splash screen was displayed.

--- app/src/main/java/com/ccakir/androidplayground/MainActivity.kt
+++ app/src/main/java/com/ccakir/androidplayground/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AndroidPlayground)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
